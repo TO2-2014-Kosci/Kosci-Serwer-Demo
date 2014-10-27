64f1fa9cd4279c988977ab99ace294ea39985351
januszekm3 <jmirosla@student.agh.edu.pl>
2014-10-27 23:46:14 +0100

    new branch (?)

--- src/to2/kosci/serwer/demo/Server.java
+++ src/to2/kosci/serwer/demo/Server.java
@@ -4,8 +4,6 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.QueueingConsumer;
-import to2.kosci.protocols.Server;
-
 import java.io.IOException;
 import java.util.LinkedList;
 /**
@@ -15,5 +13,5 @@ public class Server {
     private LinkedList<Player> players = new LinkedList<>();
     private LinkedList<Room> rooms = new LinkedList<>();
 
-    Server.Response handleRequest(Server.Request Request);
+    Response handleRequest(Request Request);
 }
