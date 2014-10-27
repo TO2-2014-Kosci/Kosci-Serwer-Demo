449a7f251f902dfedc8576c9015b7e153d85cb03
fanfilmu <fanflm@gmail.com>
2014-10-27 17:49:00 +0100

    Test commit

--- src/to2/kosci/serwer/demo/Main.java
+++ src/to2/kosci/serwer/demo/Main.java
@@ -14,7 +14,7 @@ public class Main {
                     break;
                 case "c":
                     Client client = new Client("localhost", "dice_demo_queue");
-                    client.login("Fan" + (new Random()).nextInt(32));
+                    client.login("Ean" + (new Random()).nextInt(32));
                     client.closeConnection();
             }
         }
